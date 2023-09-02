8fbe49e7737d0e15e6078869cd66b35757ca7d14
MinhNN <itptit2001@gmail.com>
2023-09-02 15:20:52 +0700

    update email

--- build/helpers/emails.js
+++ build/helpers/emails.js
@@ -35,11 +35,16 @@ var orderEmail = function () {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
+            var sum=0
+            for(var i=0; i< details[0].products; i++){
+              sum+=details[0].products[i].quantity
+            }
+            console.log("QUANTITY", sum);
             msg = {
               to: `${user.email}`,
               from: 'itptit104@gmail.com',
               subject: 'Đơn hàng của bạn đã được đặt thành công',
-              html: '<strong>Xin ch\u00e0o: <em> '+ user.name +'</em>, c\u1EA3m \u01A1n b\u1EA1n v\xEC \u0111\xE3 \u0111\u1EB7t h\xE0ng</strong>\n    <p>Sau \u0111\xE2y l\xE0 th\xF4ng tin chi ti\u1EBFt c\u1EE7a \u0111\u01A1n h\xE0ng</p>\n    <p>Th\u1eddi gian \u0111\u1eb7t h\u00e0ng : ' + details[0].created_on + '</p>\n    <p>T\u00ean ng\u01b0\u1eddi nh\u1eadn h\u00e0ng : ' + details[0].receiver_name + '</p>\n    <p>S\u1ed1 \u0111i\u1ec7n tho\u1ea1i ng\u01b0\u1eddi nh\u1eadn : ' + details[0].receiver_phone + '</p>\n    <p>\u0110\u1ecba ch\u1ec9 ng\u01b0\u1eddi nh\u1eadn : ' + details[0].address + '</p>\n    <p>S\u1ED1 l\u01B0\u1EE3ng : ' + details[0].products.length + '</p>\n    <p>Ph\u01B0\u01A1ng th\u1EE9c giao h\xE0ng : ' + details[0].shipping_type + '</p>\n    <p>Ph\xED giao h\xE0ng : ' + details[0].shipping_cost + '</p>\n    <p>T\u1ed5ng ti\u1ec1n ph\u1ea3i tr\u1ea3 : ' + details[0].order_total + '</p>\n    <p>Tr\u00e2n tr\u1ecdng,</p>\n    <p>\u0110\u1ed9i ng\u0169 BookShop.</p>'
+              html: '<strong>Xin ch\u00e0o: <em> '+ user.name +'</em>, c\u1EA3m \u01A1n b\u1EA1n v\xEC \u0111\xE3 \u0111\u1EB7t h\xE0ng</strong>\n    <p>Sau \u0111\xE2y l\xE0 th\xF4ng tin chi ti\u1EBFt c\u1EE7a \u0111\u01A1n h\xE0ng</p>\n    <p>Th\u1eddi gian \u0111\u1eb7t h\u00e0ng : ' + details[0].created_on + '</p>\n    <p>T\u00ean ng\u01b0\u1eddi nh\u1eadn h\u00e0ng : ' + details[0].receiver_name + '</p>\n    <p>S\u1ed1 \u0111i\u1ec7n tho\u1ea1i ng\u01b0\u1eddi nh\u1eadn : ' + details[0].receiver_phone + '</p>\n    <p>\u0110\u1ecba ch\u1ec9 ng\u01b0\u1eddi nh\u1eadn : ' + details[0].address + '</p>\n    <p>S\u1ED1 l\u01B0\u1EE3ng : ' + sum + '</p>\n    <p>Ph\u01B0\u01A1ng th\u1EE9c giao h\xE0ng : ' + details[0].shipping_type + '</p>\n    <p>Ph\xED giao h\xE0ng : ' + details[0].shipping_cost + '</p>\n    <p>T\u1ed5ng ti\u1ec1n ph\u1ea3i tr\u1ea3 : ' + details[0].order_total + '</p>\n    <p>Tr\u00e2n tr\u1ecdng,</p>\n    <p>\u0110\u1ed9i ng\u0169 BookShop.</p>'
             };
             _context.next = 3;
             // return _mail2.default.send(msg);
